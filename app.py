c2e876f444933c2602d3817c4afcb753cbf9d270
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2021-08-28 22:40:45 +0100

    added logging functionality

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 
 @app.route("/status")
 def doHealthCheck():
     response = {'result': 'OK - healthy'}
+    app.logger.info('Status request successful')
     return response, 200
 
 
 @app.route("/metrics")
 def getMetrics():
     response = {'UserCount': 140, 'UserCountActive': 23}
+    app.logger.info('Metrics request successful')
     return response, 200
 
 
 if __name__ == "__main__":
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
     app.run(host='0.0.0.0')
